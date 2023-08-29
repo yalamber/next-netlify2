c73ca63d9c8417b9883006c836841e5be8ee10a4
Yalamber <yalamber@mundhum.com>
2023-08-29 16:26:53 +0545

    Added request to api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const req = await fetch(
+        'https://main--coruscating-sunburst-b66de9.netlify.app/api/receive-token'
+      );
+      const data = await req.json();
+      console.log(data);
+    })();
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -109,5 +119,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
